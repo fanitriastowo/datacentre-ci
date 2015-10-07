c4e2fde50efe4bc24f655761e9a858a3cc362e66
fanitriastowo <fani.triastowo@gmail.com>
2015-10-07 09:56:39 +0700

    styling home page

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -2,9 +2,47 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Home Page</title>		
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Welcome</title>
+	<link rel="icon" href="<?php echo site_url('assets/images/favicon.ico'); ?>">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css" href="<?php echo site_url('assets/css/bootstrap.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo site_url('assets/css/bootstrap-theme.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo site_url('assets/css/main.css'); ?>">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
 </head>
 <body>
-	<h1>Hello</h1>
+	<div class="container">
+		<div class="header">
+			<nav>
+				<ul class="nav nav-pills pull-right">
+					<li role="presentation" class="active"><a href="#">Home</a></li>
+					<li role="presentation"><a href="#">About</a></li>
+					<li role="presentation"><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+			<h3 class="text-muted">Project name</h3>
+		</div>
+
+		<div class="jumbotron">
+			<h1>Jumbotron heading</h1>
+			<p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+			<p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+		</div>
+
+		<h1>This is Home View</h1>
+		<p>Berisi Data Gedung lengkap dengan detailnya</p>
+	</div>
+
+	<script type="text/javascript" src="<?php echo site_url('assets/js/jquery.min.js'); ?>"></script>
+	<script type="text/javascript" src="<?php echo site_url('assets/js/bootstrap.min.js'); ?>"></script>
 </body>
 </html>
