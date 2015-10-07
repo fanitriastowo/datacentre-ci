2f5f50ce7db88a4e2804864f29ed61b5f8592913
fanitriastowo <fani.triastowo@gmail.com>
2015-10-07 10:00:36 +0700

    finish implement templating for navbar and other

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,36 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php $this->load->view('template/css'); ?>
 	<title>Welcome</title>
-	<link rel="icon" href="<?php echo site_url('assets/images/favicon.ico'); ?>">
-	<!-- Bootstrap -->
-	<link rel="stylesheet" type="text/css" href="<?php echo site_url('assets/css/bootstrap.min.css'); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo site_url('assets/css/bootstrap-theme.min.css'); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo site_url('assets/css/main.css'); ?>">
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->
-
 </head>
 <body>
 	<div class="container">
-		<div class="header">
-			<nav>
-				<ul class="nav nav-pills pull-right">
-					<li role="presentation" class="active"><a href="#">Home</a></li>
-					<li role="presentation"><a href="#">About</a></li>
-					<li role="presentation"><a href="#">Contact</a></li>
-				</ul>
-			</nav>
-			<h3 class="text-muted">Project name</h3>
-		</div>
+		<?php $this->load->view('template/navbar'); ?>
 
 		<div class="jumbotron">
 			<h1>Jumbotron heading</h1>
@@ -42,7 +18,6 @@
 		<p>Berisi Data Gedung lengkap dengan detailnya</p>
 	</div>
 
-	<script type="text/javascript" src="<?php echo site_url('assets/js/jquery.min.js'); ?>"></script>
-	<script type="text/javascript" src="<?php echo site_url('assets/js/bootstrap.min.js'); ?>"></script>
+<?php $this->load->view('template/js'); ?>
 </body>
 </html>
