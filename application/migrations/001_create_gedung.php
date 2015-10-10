23730ca6428efd34fe78320f0fd84a62b4882511
Fani Triastowo <fani.triastowo@gmail.com>
2015-10-10 23:26:39 +0700

    fixing migration for gedung

--- application/migrations/001_create_gedung.php
+++ application/migrations/001_create_gedung.php
@@ -12,12 +12,18 @@ class Migration_Create_Gedung extends CI_Migration {
 				  `tahun_survey` DATE NOT NULL,
 				  `luas` DOUBLE NOT NULL DEFAULT 0,
 				  `lokasi` TEXT NULL,
-				  `jenis_gedung` ENUM(
-				  				'1_LANTAI', '2_LANTAI', '3_LANTAI', 
-				  				'4_LANTAI', '5_LANTAI', 'LAIN_LAIN') NOT NULL,
+				  `jenis_gedung` VARCHAR(50) NOT NULL,
 				  PRIMARY KEY (`id`) 
 				  )";
+
+		$insert_teknik = "INSERT INTO gedung SET kode = 'R', nama = 'Teknik', fungsi = 'Perkuliahan, Laboraturium', tahun_berdiri = '2014-07-15', tahun_survey = '2014-07-15', luas = 4000.0, lokasi = 'Kampus 1 UMP', jenis_gedung = '5 Lantai'";
+
+		$insert_kedokteran = "INSERT INTO gedung SET kode = 'Q', nama = 'Kedokteran', fungsi = 'Perkuliahan, Laboraturium, Penelitian', tahun_berdiri = '2012-07-15', tahun_survey = '2013-07-15', luas = 4000.0, lokasi = 'Kampus 1 UMP', jenis_gedung = '5 Lantai'";
+
 		$this->db->query($query);
+		$this->db->query($insert_teknik);
+		$this->db->query($insert_kedokteran);
+
 	}
 	public function down() {
 		$this->dbforge->drop_table('gedung');
--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -5,5 +5,5 @@
 			<li role="presentation" id="gedung"><?php echo anchor('gedung', 'Gedung') ?></li>
 		</ul>
 	</nav>
-	<h3 class="text-muted">Datacentre</h3>
+	<h3 class="text-muted"><a href="<?php echo site_url('/'); ?>" style="text-decoration : none">Datacentre</a></h3>
 </div>
