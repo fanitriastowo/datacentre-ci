043008580d715595040d0af0e8ad18f9cca9d0d4
fanitriastowo <fani.triastowo@gmail.com>
2015-10-18 02:11:37 +0700

    change user profile form

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -12,60 +12,35 @@
 		<p class="text-warning">*Silahkan inputkan form-form berikut ini. apabila ada kesulitan menginputkan silahkan hubungi Pusat Perencanaan Pengembangan di Ekstensi: <kbd>334</kbd></p>
 		<div class="panel panel-default">
 			<div class="panel-body">
-
+			
 			<?php echo form_open('user/profile/insert', 'class="form-horizontal"'); ?>
+			
+			<div class="row">
+			<div class="col-md-6">
 
 				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-3">
+					<div class="col-sm-offset-1 col-sm-4">
 						<label for="insert_kode" class="control-label">Kode</label>
 						<input title="Kode Huruf Gedung" type="text" name="kode" class="form-control" id="insert_kode" placeholder="Kode Gedung" autofocus>
 					</div>
 				</div>
 
 				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-4">
+					<div class="col-sm-offset-1 col-sm-10">
 						<label for="insert_nama" class="control-label">Nama</label>
 						<input title="Nama Gedung" type="text" name="nama" class="form-control" id="insert_nama" placeholder="Nama Gedung">
 					</div>
 				</div>
 
 				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-5">
-						<label for="insert_fungsi" class="control-label">Fungsi</label>
-						<textarea title="Fungsi Gedung" name="fungsi" class="form-control" id="insert_fungsi" placeholder="Fungsi Gedung" rows="3"style="resize : none;"></textarea>
-					</div>
-				</div>
-
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-4">
-						<label for="insert_tahun_berdiri" class="control-label">Tahun Berdiri</label>
-						<input title="Tahun Berdiri Gedung" type="text" name="tahun_berdiri" class="form-control" id="insert_tahun_berdiri" placeholder="Tahun Berdiri Gedung">
-					</div>
-				</div>
-
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-4">
-						<label for="insert_tahun_survey" class="control-label">Tahun Survey</label>
-						<input title="Tahun Survey Gedung" type="text" name="tahun_survey" class="form-control" id="insert_tahun_survey" placeholder="Tahun Survey Gedung">
-					</div>
-				</div>
-
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-4">
-						<label for="insert_luas" class="control-label">Luas</label>
-						<input title="Luas Bangunan dalam m2" type="text" name="luas" class="form-control" id="insert_luas" placeholder="Luas Bangunan dalam m2">
-					</div>
-				</div>
-
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-5">
+					<div class="col-sm-offset-1 col-sm-10">
 						<label for="insert_lokasi" class="control-label">Lokasi</label>
 						<textarea title="Lokasi Gedung misal Kampus 1 UMP Dukuhwaluh" name="lokasi" class="form-control" id="insert_lokasi" placeholder="Lokasi Gedung" rows="3" style="resize : none;"></textarea>
 					</div>
 				</div>
 
 				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-3">
+					<div class="col-sm-offset-1 col-sm-10">
 						<label for="insert_jenis_gedung" class="control-label">Jenis Gedung</label>
 						<select class="form-control" id="insert_jenis_gedung">
 							<option value="1_Lantai">1 Lantai</option>
@@ -77,12 +52,45 @@
 						</select>
 					</div>
 				</div>
-	
+			</div>
+
+			<div class="col-md-6">
 				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-2">
+					<div class="col-sm-offset-1 col-sm-6">
+						<label for="insert_tahun_berdiri" class="control-label">Tahun Berdiri</label>
+						<input title="Tahun Berdiri Gedung" type="text" name="tahun_berdiri" class="form-control" id="insert_tahun_berdiri" placeholder="Tahun Berdiri Gedung">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="col-sm-offset-1 col-sm-6">
+						<label for="insert_tahun_survey" class="control-label">Tahun Survey</label>
+						<input title="Tahun Survey Gedung" type="text" name="tahun_survey" class="form-control" id="insert_tahun_survey" placeholder="Tahun Survey Gedung">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="col-sm-offset-1 col-sm-10">
+						<label for="insert_fungsi" class="control-label">Fungsi</label>
+						<textarea title="Fungsi Gedung" name="fungsi" class="form-control" id="insert_fungsi" placeholder="Fungsi Gedung" rows="3"style="resize : none;"></textarea>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="col-sm-offset-1 col-sm-6">
+						<label for="insert_luas" class="control-label">Luas</label>
+						<input title="Luas Bangunan dalam m2" type="text" name="luas" class="form-control" id="insert_luas" placeholder="Luas Bangunan dalam m2">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="col-sm-offset-7 col-sm-4">
 						<button type="submit" class="btn btn-lg btn-primary btn-block">Simpan</button>
 					</div>
 				</div>
+			</div>
+			</div>
+	
 			<?php echo form_close(); ?>
 
 			</div>
