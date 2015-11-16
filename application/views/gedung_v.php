91d00b95e3799785ee22de7d9cae4eab48d2e358
Fani Triastowo <fani.triastowo@gmail.com>
2015-11-16 07:29:28 +0700

    empty gedung message

--- application/views/gedung_v.php
+++ application/views/gedung_v.php
@@ -9,18 +9,24 @@
 		<?php $this->load->view('template/navbar'); ?>
 
 		<div class="row">
-		<?php foreach ($gedungs as $gedung): ?>
-			<div class="col-xs-6 col-md-3">
-				<div class="thumbnail">
-					<a href="<?php echo site_url('gedung/detail/'. $gedung->id); ?>">
-						<img src="<?php echo site_url('images/blank.jpg'); ?>" alt="<?php echo $gedung->nama; ?>">
-					</a>
-					<div class="caption text-center">
-						<h3><?php echo $gedung->nama; ?></h3>
+		<?php if (count($gedungs)): ?>
+			<?php foreach ($gedungs as $gedung): ?>
+				<div class="col-xs-6 col-md-3">
+					<div class="thumbnail">
+						<a href="<?php echo site_url('gedung/detail/'. $gedung->id); ?>">
+							<img src="<?php echo site_url('images/blank.jpg'); ?>" alt="<?php echo $gedung->nama; ?>">
+						</a>
+						<div class="caption text-center">
+							<h3><?php echo $gedung->nama; ?></h3>
+						</div>
 					</div>
 				</div>
-			</div>
-		<?php endforeach ?>
+			<?php endforeach ?>
+		<?php else: ?>
+		<div class="col-sm-12 text-center">
+			<span class="label label-danger">Ooppss! Belum terdapat data gedung</span>
+		</div>
+		<?php endif ?>
 		</div>
 
 	</div>
