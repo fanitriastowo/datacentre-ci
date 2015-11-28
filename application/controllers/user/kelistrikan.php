f3290527c5389b37f94b848c9e6e79476b8e2309
fanitriastowo <fani.triastowo@gmail.com>
2015-11-28 23:31:10 +0700

    finish process kelistrikan form

--- application/controllers/user/kelistrikan.php
+++ application/controllers/user/kelistrikan.php
@@ -4,9 +4,80 @@ class Kelistrikan extends User_Controller {
 
 	function __construct() {
 		parent::__construct();
+		$this->load->model('kelistrikan_m');
 	}
 
 	public function insert() {
-		
+		$user = $this->ion_auth->user()->row();
+		$kelistrikan = $this->kelistrikan_m->get($user->id, TRUE);
+
+		// Update
+		if (count($kelistrikan)) {
+			$data = array(
+				'instalasi_kabel_panjang' => $this->input->post('instalasi_kabel_panjang'),
+				'instalasi_kabel_jenis' => $this->input->post('instalasi_kabel_jenis'),
+				'instalasi_kabel_kondisi' => $this->input->post('instalasi_kabel_kondisi'),
+				'lampu_jumlah' => $this->input->post('lampu_jumlah'),
+				'lampu_jumlah_baik' => $this->input->post('lampu_jumlah_baik'),
+				'lampu_jumlah_rusak' => $this->input->post('lampu_jumlah_rusak'),
+				'stop_kontak_jumlah' => $this->input->post('stop_kontak_jumlah'),
+				'stop_kontak_jumlah_baik' => $this->input->post('stop_kontak_jumlah_baik'),
+				'stop_kontak_jumlah_rusak' => $this->input->post('stop_kontak_jumlah_rusak'),
+				'saklar_jumlah' => $this->input->post('saklar_jumlah'),
+				'saklar_jumlah_baik' => $this->input->post('saklar_jumlah_baik'),
+				'saklar_jumlah_rusak' => $this->input->post('saklar_jumlah_rusak'),
+				'instalasi_listrik_kondisi' => $this->input->post('instalasi_listrik_kondisi'),
+				'installasi_kable_LAN_panjang' => $this->input->post('installasi_kable_LAN_panjang'),
+				'installasi_kable_LAN_kondisi' => $this->input->post('installasi_kable_LAN_kondisi'),
+				'switch_jumlah' => $this->input->post('switch_jumlah'),
+				'switch_jumlah_baik' => $this->input->post('switch_jumlah_baik'),
+				'switch_jumlah_rusak' => $this->input->post('switch_jumlah_rusak')
+			);
+			$this->form_validation->set_rules($this->kelistrikan_m->rules);
+			
+			if ($this->form_validation->run() == TRUE) {
+				$this->kelistrikan_m->save($data, $atap->id);
+				$this->session->set_flashdata('notif', 'Update Kelistrikan Berhasil!');
+				redirect('user/profile#tab-kelistrikan');
+			} else {
+				$this->session->set_flashdata('error', validation_errors());
+				redirect('user/profile#tab-kelistrikan');
+			}
+
+		// Insert
+		} else {
+			$data = array(
+				'id' => $user->id,
+				'instalasi_kabel_panjang' => $this->input->post('instalasi_kabel_panjang'),
+				'instalasi_kabel_jenis' => $this->input->post('instalasi_kabel_jenis'),
+				'instalasi_kabel_kondisi' => $this->input->post('instalasi_kabel_kondisi'),
+				'lampu_jumlah' => $this->input->post('lampu_jumlah'),
+				'lampu_jumlah_baik' => $this->input->post('lampu_jumlah_baik'),
+				'lampu_jumlah_rusak' => $this->input->post('lampu_jumlah_rusak'),
+				'stop_kontak_jumlah' => $this->input->post('stop_kontak_jumlah'),
+				'stop_kontak_jumlah_baik' => $this->input->post('stop_kontak_jumlah_baik'),
+				'stop_kontak_jumlah_rusak' => $this->input->post('stop_kontak_jumlah_rusak'),
+				'saklar_jumlah' => $this->input->post('saklar_jumlah'),
+				'saklar_jumlah_baik' => $this->input->post('saklar_jumlah_baik'),
+				'saklar_jumlah_rusak' => $this->input->post('saklar_jumlah_rusak'),
+				'instalasi_listrik_kondisi' => $this->input->post('instalasi_listrik_kondisi'),
+				'installasi_kable_LAN_panjang' => $this->input->post('installasi_kable_LAN_panjang'),
+				'installasi_kable_LAN_kondisi' => $this->input->post('installasi_kable_LAN_kondisi'),
+				'switch_jumlah' => $this->input->post('switch_jumlah'),
+				'switch_jumlah_baik' => $this->input->post('switch_jumlah_baik'),
+				'switch_jumlah_rusak' => $this->input->post('switch_jumlah_rusak')
+			);
+			
+			$this->form_validation->set_rules($this->kelistrikan_m->rules);
+			
+			if ($this->form_validation->run() == TRUE) {
+				$this->kelistrikan_m->save_kelistrikan($data);
+				$this->session->set_flashdata('notif', 'Insert Kelistrikan Berhasil!');
+				redirect('user/profile#tab-kelistrikan');
+			} else {
+				$this->session->set_flashdata('error', validation_errors());
+				redirect('user/profile#tab-kelistrikan');
+			}			
+		}
 	}
 }
--- application/models/kelistrikan_m.php
+++ application/models/kelistrikan_m.php
@@ -4,10 +4,108 @@ class Kelistrikan_m extends MY_Model {
 
 	protected $_table_name = 'kelistrikan';
 	protected $_order_by = 'id';
-	public $rules = array();
+	public $rules = array(
+		'instalasi_kabel_panjang' => array(
+			'field' => 'instalasi_kabel_panjang', 
+			'label' => 'Panjang Instalasi Kabel',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'instalasi_kabel_jenis' => array(
+			'field' => 'instalasi_kabel_jenis', 
+			'label' => 'Jenis Instalasi Kabel',
+			'rules' => 'trim|xss_clean'
+		),
+		'instalasi_kabel_kondisi' => array(
+			'field' => 'instalasi_kabel_kondisi', 
+			'label' => 'Kondisi Instalasi Kabel',
+			'rules' => 'trim|xss_clean'
+		),
+		'lampu_jumlah' => array(
+			'field' => 'lampu_jumlah', 
+			'label' => 'Jumlah Total Lampu',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'lampu_jumlah_baik' => array(
+			'field' => 'lampu_jumlah_baik', 
+			'label' => 'Jumlah Lampu Kondisi Baik',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'lampu_jumlah_rusak' => array(
+			'field' => 'lampu_jumlah_rusak', 
+			'label' => 'Jumlah Lampu Kondisi Rusak',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'stop_kontak_jumlah' => array(
+			'field' => 'stop_kontak_jumlah', 
+			'label' => 'Jumlah Total Stop Kontak',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'stop_kontak_jumlah_baik' => array(
+			'field' => 'stop_kontak_jumlah_baik', 
+			'label' => 'Jumlah Stop Kontak Kondisi Baik',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'stop_kontak_jumlah_rusak' => array(
+			'field' => 'stop_kontak_jumlah_rusak', 
+			'label' => 'Jumlah Stop Kontak Kondisi Rusak',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'saklar_jumlah' => array(
+			'field' => 'saklar_jumlah', 
+			'label' => 'Jumlah Total Saklar',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'saklar_jumlah_baik' => array(
+			'field' => 'saklar_jumlah_baik', 
+			'label' => 'Jumlah Saklar Kondisi Baik',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'saklar_jumlah_rusak' => array(
+			'field' => 'saklar_jumlah_rusak', 
+			'label' => 'Jumlah Saklar Kondisi Rusak',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'instalasi_listrik_kondisi' => array(
+			'field' => 'instalasi_listrik_kondisi', 
+			'label' => 'Kondisi Instalasi Listrik',
+			'rules' => 'trim|xss_clean'
+		),
+		'installasi_kable_LAN_panjang' => array(
+			'field' => 'installasi_kable_LAN_panjang', 
+			'label' => 'Panjang Instalasi Kabel LAN',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'installasi_kable_LAN_kondisi' => array(
+			'field' => 'installasi_kable_LAN_kondisi', 
+			'label' => 'Kondisi Instalasi Kabel LAN',
+			'rules' => 'trim|xss_clean'
+		),
+		'switch_jumlah' => array(
+			'field' => 'switch_jumlah', 
+			'label' => 'Jumlah Total Switch',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'switch_jumlah_baik' => array(
+			'field' => 'switch_jumlah_baik', 
+			'label' => 'Jumlah Switch Kondisi Baik',
+			'rules' => 'trim|xss_clean|integer'
+		),
+		'switch_jumlah_rusak' => array(
+			'field' => 'switch_jumlah_rusak', 
+			'label' => 'Jumlah Switch Kondisi Rusak',
+			'rules' => 'trim|xss_clean|integer'
+		)
+	);
 
 	function __construct() {
 		parent::__construct();
+	}
+
+	public function save_kelistrikan($data) {
+		$this->db->set($data);
+		$this->db->insert($this->_table_name);
+		$id = $this->db->insert_id();
+		return $id;
 	}
 
 	public function get_new() {
