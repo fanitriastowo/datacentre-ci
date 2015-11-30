adf5c4e71f4a813fecd936050cf4aab419e70aa6
fanitriastowo <fani.triastowo@gmail.com>
2015-11-30 22:52:50 +0700

    finish process plafon form user

--- application/controllers/user/plafon.php
+++ application/controllers/user/plafon.php
@@ -4,5 +4,61 @@ class Plafon extends User_Controller {
 
 	function __construct() {
 		parent::__construct();
+		$this->load->model('plafon_m');
+	}
+
+	public function insert() {
+		$user = $this->ion_auth->user()->row();
+		$plafon = $this->plafon_m->get($user->id, TRUE);
+
+		// Update
+		if (count($plafon)) {
+			$data = array(
+				'lisplang_panjang' => $this->input->post('lisplang_panjang'),
+				'lisplang_bahan' => $this->input->post('lisplang_bahan'),
+				'lisplang_kondisi' => $this->input->post('lisplang_kondisi'),
+				'rangka_plafon_panjang' => $this->input->post('rangka_plafon_panjang'),
+				'rangka_plafon_bahan' => $this->input->post('rangka_plafon_bahan'),
+				'rangka_plafon_kondisi' => $this->input->post('rangka_plafon_kondisi'),
+				'plafon_panjang' => $this->input->post('plafon_panjang'),
+				'plafon_bahan' => $this->input->post('plafon_bahan'),
+				'plafon_kondisi' => $this->input->post('plafon_kondisi')
+			);
+
+			$this->form_validation->set_rules($this->plafon_m->rules);
+			if ($this->form_validation->run() == TRUE) {
+				$this->plafon_m->save($data, $plafon->id);
+				$this->session->set_flashdata('notif', 'Update Plafon Berhasil!');
+				redirect('user/profile#tab-plafon');
+			} else {
+				$this->session->set_flashdata('error', validation_errors());
+				redirect('user/profile#tab-plafon');
+			}
+
+		// Insert
+		} else {
+			$data = array(
+				'id' => $user->id,
+				'lisplang_panjang' => $this->input->post('lisplang_panjang'),
+				'lisplang_bahan' => $this->input->post('lisplang_bahan'),
+				'lisplang_kondisi' => $this->input->post('lisplang_kondisi'),
+				'rangka_plafon_panjang' => $this->input->post('rangka_plafon_panjang'),
+				'rangka_plafon_bahan' => $this->input->post('rangka_plafon_bahan'),
+				'rangka_plafon_kondisi' => $this->input->post('rangka_plafon_kondisi'),
+				'plafon_panjang' => $this->input->post('plafon_panjang'),
+				'plafon_bahan' => $this->input->post('plafon_bahan'),
+				'plafon_kondisi' => $this->input->post('plafon_kondisi')
+			);
+
+			$this->form_validation->set_rules($this->plafon_m->rules);
+			if ($this->form_validation->run() == TRUE) {
+				$this->plafon_m->save_plafon($data);
+				$this->session->set_flashdata('notif', 'Insert Plafon Berhasil!');
+				redirect('user/profile#tab-plafon');
+			} else {
+				$this->session->set_flashdata('error', validation_errors());
+				redirect('user/profile#tab-plafon');
+			}
+		}
 	}
 }
--- application/models/plafon_m.php
+++ application/models/plafon_m.php
@@ -4,7 +4,53 @@ class Plafon_m extends MY_Model {
 
 	protected $_table_name = 'plafon';
 	protected $_order_by = 'id';
-	public $rules = array();
+	public $rules = array(
+		'lisplang_panjang' => array(
+			'field' => 'lisplang_panjang', 
+			'label' => 'Panjang Lisplang',
+			'rules' => 'trim|xss_clean|numeric'
+		),
+		'lisplang_bahan' => array(
+			'field' => 'lisplang_bahan', 
+			'label' => 'Bahan Lisplang',
+			'rules' => 'trim|xss_clean'
+		),
+		'lisplang_kondisi' => array(
+			'field' => 'lisplang_kondisi', 
+			'label' => 'Kondisi Lisplang',
+			'rules' => 'trim|xss_clean'
+		),
+		'rangka_plafon_panjang' => array(
+			'field' => 'rangka_plafon_panjang', 
+			'label' => 'Panjang Rangka Plafon',
+			'rules' => 'trim|xss_clean|numeric'
+		),
+		'rangka_plafon_bahan' => array(
+			'field' => 'rangka_plafon_bahan', 
+			'label' => 'Bahan Rangka Plafon',
+			'rules' => 'trim|xss_clean'
+		),
+		'rangka_plafon_kondisi' => array(
+			'field' => 'rangka_plafon_kondisi', 
+			'label' => 'Kondisi Rangka Plafon',
+			'rules' => 'trim|xss_clean'
+		),
+		'plafon_panjang' => array(
+			'field' => 'plafon_panjang', 
+			'label' => 'Panjang Plafon',
+			'rules' => 'trim|xss_clean|numeric'
+		),
+		'plafon_bahan' => array(
+			'field' => 'plafon_bahan', 
+			'label' => 'Bahan Plafon',
+			'rules' => 'trim|xss_clean'
+		),
+		'plafon_kondisi' => array(
+			'field' => 'plafon_kondisi', 
+			'label' => 'Kondisi Plafon',
+			'rules' => 'trim|xss_clean'
+		)
+	);
 
 	function __construct() {
 		parent::__construct();
@@ -22,5 +68,12 @@ class Plafon_m extends MY_Model {
 		$plafon->plafon_bahan = '';
 		$plafon->plafon_kondisi = 'BAIK';
 		return $plafon;
+	}
+
+	public function save_plafon($data) {
+		$this->db->set($data);
+		$this->db->insert($this->_table_name);
+		$id = $this->db->insert_id();
+		return $id;
 	}
 }
