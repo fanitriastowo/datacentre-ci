3eade428dca1c3ceae4d2f8116bed340bb7d7d48
fanitriastowo <fani.triastowo@gmail.com>
2015-12-01 12:39:33 +0700

    add pondasi migration

---
+++ application/migrations/008_create_pondasi.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Migration_Create_pondasi extends CI_Migration {
+
+	public function up(){
+		$query = "CREATE TABLE pondasi (
+			id INT(11) unsigned NOT NULL,
+			sloof_panjang DOUBLE,
+			sloof_bahan VARCHAR(100),
+			sloof_kondisi VARCHAR(100),
+			PRIMARY KEY (id),
+			CONSTRAINT FK_PONDASI_ID FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE NO ACTION)
+		";
+		$this->db->query($query);
+	}
+
+	public function down(){
+		$this->db_forge->drop_table('pondasi');
+	}
+}
